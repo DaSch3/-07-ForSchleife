9c0030613fe86cbdafc7eb1cc9c314c0fd8085ff
David Schmitz <d.schmitz@dasch3.de>
2021-09-04 19:25:33 +0200

    Projektdateien hinzufügen.

---
+++ ForSchleife/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForSchleife
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //For-Schleifen
+            for(int i = 5; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
